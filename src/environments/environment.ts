fddebbdf2eb466b617e6fcbd55c0fd4061fc10c0
Shah-Shishir <shahshishir.09@gmail.com>
2023-09-21 21:05:17 +0600

    initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'fir-auth-ad86e',
+    appId: '1:782130703543:web:af8807742bb75a1ca33132',
+    storageBucket: 'fir-auth-ad86e.appspot.com',
+    apiKey: 'AIzaSyA8h6loa5hDWiPOzyqBJL5C6u0DnO6PpZI',
+    authDomain: 'fir-auth-ad86e.firebaseapp.com',
+    messagingSenderId: '782130703543',
+    measurementId: 'G-JSRJL42XW3',
+  },
+};
